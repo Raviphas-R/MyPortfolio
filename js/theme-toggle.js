2956662b91e898d7217a532ce73abe8050035fa1
Raviphas-R <iamnice2020@gmail.com>
2022-12-31 15:52:45 +0700

    fix prefers-color-scheme in css to work properly.

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,3 +1,9 @@
+let toggle = document.getElementById("toggle-theme");
+let container = document.getElementById("container-toggle");
+let sun = document.getElementById("sun-theme");
+let moon = document.getElementById("moon-theme");
+let state = false;
+
 (window.applyThemeSetting = function (e) {
   const t = window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "dark"
@@ -6,16 +12,18 @@
     r = document.documentElement.getAttribute("data-user-theme");
   if (o) {
     o !== r && document.documentElement.setAttribute("data-user-theme", o);
+    if (o == "dark") {
+      state = true;
+      container.classList.add("dark");
+      toggle.classList.add("toggled");
+      sun.classList.remove("animated");
+      moon.classList.add("animated");
+    }
   }
 }),
   window.applyThemeSetting();
-/* theme toggle button */
-let toggle = document.getElementById("toggle-theme");
-let container = document.getElementById("container-toggle");
-let sun = document.getElementById("sun-theme");
-let moon = document.getElementById("moon-theme");
-let state = false;
 
+/* theme toggle button */
 toggle.addEventListener("click", () => {
   state = !state;
   if (state) {
