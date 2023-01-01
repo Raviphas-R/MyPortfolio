1e62105de93ebb64d8007bad59039843e6a58c0c
Raviphas-R <iamnice2020@gmail.com>
2023-01-01 12:53:37 +0700

    add animation to dropdown navbar. wait for add animation when open and close nav-bar.

--- js/navbar-btn.js
+++ js/navbar-btn.js
@@ -1,9 +1,15 @@
 $("#navbar-toggle-btn").click(function () {
   $("#nav-bar").toggleClass("hide");
+  $("#nav-bar").addClass("navbar-toggled");
+  $("#nav-bar > ul").addClass("navbar-toggled");
 });
 
 $(window).on("resize", function () {
   if ($(this).width() >= 880) {
     $("#nav-bar").addClass("hide");
+  }
+  if ($(this).width() >= 880) {
+    $("#nav-bar").removeClass("navbar-toggled");
+    $("#nav-bar > ul").removeClass("navbar-toggled");
   }
 });
