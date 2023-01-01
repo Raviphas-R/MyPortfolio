36b9420040692ab465b2eeb2feeea3f486f12947
Raviphas-R <iamnice2020@gmail.com>
2023-01-01 16:39:05 +0700

    add animation close and open nav-bar

--- js/navbar-btn.js
+++ js/navbar-btn.js
@@ -1,15 +1,31 @@
 $("#navbar-toggle-btn").click(function () {
-  $("#nav-bar").toggleClass("hide");
-  $("#nav-bar").addClass("navbar-toggled");
-  $("#nav-bar > ul").addClass("navbar-toggled");
+  $("#icon-menu").toggleClass("fa-solid fa-bars");
+  $("#icon-menu").toggleClass("fa-solid fa-xmark");
+  $("#nav-bar").removeClass("hide");
+  let navToggle = $("#nav-bar").attr("class");
+  switch (navToggle) {
+    case "navbar-container":
+      $("#nav-bar").addClass("navbar-open");
+      break;
+
+    case "navbar-container navbar-open":
+      $("#nav-bar").removeClass("navbar-open");
+      $("#nav-bar").addClass("navbar-close");
+      break;
+
+    case "navbar-container navbar-close":
+      $("#nav-bar").addClass("navbar-open");
+      $("#nav-bar").removeClass("navbar-close");
+      break;
+  }
 });
 
 $(window).on("resize", function () {
   if ($(this).width() >= 880) {
     $("#nav-bar").addClass("hide");
-  }
-  if ($(this).width() >= 880) {
-    $("#nav-bar").removeClass("navbar-toggled");
-    $("#nav-bar > ul").removeClass("navbar-toggled");
+    $("#nav-bar").removeClass("navbar-close navbar-open");
+    $("#nav-bar > ul").removeClass("navbar-open");
+    $("#icon-menu").removeClass("fa-solid fa-xmark");
+    $("#icon-menu").addClass("fa-solid fa-bars");
   }
 });
