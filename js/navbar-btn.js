2fa773b47168c602ddd93ca0c18e82c954ae730c
Raviphas-R <iamnice2020@gmail.com>
2023-01-03 14:18:26 +0700

    change navbar.js file to reuse by using function. and add background to home

--- js/navbar-btn.js
+++ js/navbar-btn.js
@@ -1,42 +1,48 @@
+let iconMenu = $("#icon-menu");
+let navBar = $("#nav-bar");
+
+let closeMenu = () => {
+  iconMenu.removeClass("fa-xmark");
+  iconMenu.addClass("fa-bars");
+};
+
+let toggleMenu = (target, removeclass, addclass) => {
+  $(target).removeClass(removeclass);
+  $(target).addClass(addclass);
+};
+
 $("#navbar-toggle-btn").click(function () {
-  $("#icon-menu").toggleClass("fa-solid fa-bars");
-  $("#icon-menu").toggleClass("fa-solid fa-xmark");
-  $("#nav-bar").removeClass("hide");
-  let navToggle = $("#nav-bar").attr("class");
+  iconMenu.toggleClass("fa-bars");
+  iconMenu.toggleClass("fa-xmark");
+  navBar.removeClass("hide");
+  let navToggle = navBar.attr("class");
   switch (navToggle) {
     case "navbar-container":
-      $("#nav-bar").addClass("navbar-open");
+      navBar.addClass("navbar-open");
       break;
 
     case "navbar-container navbar-open":
-      $("#nav-bar").removeClass("navbar-open");
-      $("#nav-bar").addClass("navbar-close");
+      toggleMenu(navBar, "navbar-open", "navbar-close");
       break;
 
     case "navbar-container navbar-close":
-      $("#nav-bar").addClass("navbar-open");
-      $("#nav-bar").removeClass("navbar-close");
+      toggleMenu(navBar, "navbar-close", "navbar-open");
       break;
   }
 });
 
-$(".nav-list").click(function () {
-  let navToggle = $("#nav-bar").attr("class");
+$(".nav-list a").click(function () {
+  let navToggle = navBar.attr("class");
   if (navToggle == "navbar-container navbar-open") {
-    $("#nav-bar").removeClass("navbar-open");
-    $("#nav-bar").addClass("navbar-close");
-    $("#icon-menu").removeClass("fa-solid fa-xmark");
-    $("#icon-menu").addClass("fa-solid fa-bars");
+    toggleMenu(navBar, "navbar-open", "navbar-close");
+    closeMenu();
   }
 });
 
 $(window).on("resize", function () {
   if ($(this).width() >= 940) {
-    $("#nav-bar").addClass("hide");
-    $("#nav-bar").removeClass("navbar-close navbar-open");
-
-    $("#nav-bar > ul").removeClass("navbar-open");
-    $("#icon-menu").removeClass("fa-solid fa-xmark");
-    $("#icon-menu").addClass("fa-solid fa-bars");
+    navBar.addClass("hide");
+    navBar.removeClass("navbar-close navbar-open");
+    closeMenu();
   }
 });
