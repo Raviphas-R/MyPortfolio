cf9b5a22d3bbfb64a81ecf7ca35e9cfab7f85853
Raviphas-R <iamnice2020@gmail.com>
2023-01-03 15:05:49 +0700

    add text typing to Home screen.

--- js/typing.js
+++ js/typing.js
@@ -1,58 +1,39 @@
-var TxtType = function (el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = "";
-  this.tick();
-  this.isDeleting = false;
-};
+const texts = [
+  "Flexible.",
+  "Reliable.",
+  "Problem-solver.",
+  "Supportive.",
+  "Team-player.",
+  "Adaptive.",
+];
+let countTyping = 0;
+let indexTyping = 0;
+let decrementTyping = 0;
+let currentText = "";
+let letter = "";
 
-TxtType.prototype.tick = function () {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+function sleep(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+const typeWrite = async () => {
+  if (countTyping == texts.length) {
+    countTyping = 0;
   }
-
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
-  var that = this;
-  var delta = 200 - Math.random() * 100;
-
-  if (this.isDeleting) {
-    delta /= 2;
-  }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function () {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function () {
-  var elements = document.getElementsByClassName("typewrite");
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-type");
-    var period = elements[i].getAttribute("data-period");
-    if (toRotate) {
-      new TxtType(elements[i], JSON.parse(toRotate), period);
+  currentWord = texts[countTyping];
+  currentLetter = currentWord.slice(0, ++indexTyping);
+  document.querySelector(".typing").textContent = currentLetter;
+  if (indexTyping == currentWord.length) {
+    await sleep(1500);
+    while (indexTyping > 0) {
+      currentLetter = currentWord.slice(0, --indexTyping);
+      document.querySelector(".typing").textContent = currentLetter;
+      await sleep(50);
     }
+    countTyping++;
+    indexTyping = 0;
+    await sleep(500);
   }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-  document.body.appendChild(css);
+  setTimeout(typeWrite, Math.random() * 200 + 50);
 };
+typeWrite();
