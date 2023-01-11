d15b6ca639540032ce9c6cbc2f2488f594139b8a
Raviphas-R <iamnice2020@gmail.com>
2023-01-11 16:57:28 +0700

    add Home content

---
+++ js/scrolling.js
@@ -0,0 +1,21 @@
+//delete #tag when refresh
+window.onload = function () {
+  window.location.hash = "";
+};
+
+//detect user scrolling
+let scrolling = (element) => {
+  $(window).scroll(function () {
+    let top_of_element = $(element).offset().top;
+    let bottom_of_element = $(element).offset().top + $(element).outerHeight();
+    let bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    let top_of_screen = $(window).scrollTop();
+
+    bottom_of_screen > top_of_element && top_of_screen < bottom_of_element
+      ? $(element).addClass("visible")
+      : $(element).removeClass("visible");
+  });
+};
+
+scrolling(".about-me");
+scrolling(".about-me-content");
